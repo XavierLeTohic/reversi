328fa7cffc3c493af8d30fa29b062a99b57c4b0e
Xavier Le Tohic <xavier.letohic@gmail.com>
2018-11-28 12:38:27 +0100

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    "airbnb-base",
+    "plugin:vue/recommended"
+  ],
+  // required to lint *.vue files
+  plugins: [
+    'vue'
+  ],
+  // add your custom rules here
+  rules: {
+    "no-console": "off"
+  }
+}
---
+++ nuxt.config.js
@@ -0,0 +1,16 @@
+export default {
+  mode: 'spa',
+  build: {
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/,
+        });
+      }
+    },
+  },
+};
